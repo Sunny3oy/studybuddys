afe03894e3ff8e665d69363ec87cc48b3c9783ba
Andy Wang <andywang219@yahoo.com>
2018-10-15 16:10:03 -0400

    Made SignUp a Stateful Component

--- src/Home/SignUp.js
+++ src/Home/SignUp.js
@@ -1,87 +1,95 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
 import './Home.css';
 
-const SignUp = () => {
-    return (
-        <div data-aos = "" className = "Home" style = {{backgroundColor:'#F5F5F5'}}>
-            <div className="signIn" 
-                data-aos="fade-down"  
-                data-aos-easing="linear" 
-                data-aos-duration="500">
-            <Typography component="h2" variant="display2" gutterBottom style={{color:'black'}}>
-                Sign Up To Be A Buddy Today
-            </Typography>
-                <form>
-                    < TextField
-                        className = ""
-                        placeholder = "First Name" 
-                        name = 'fName' 
-                    />
+class SignUp extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            
+        }
+    }
+    render() {
+        return (
+            <div data-aos = "" className = "Home" style = {{backgroundColor:'#F5F5F5'}}>
+                <div className="signIn" 
+                    data-aos="fade-down"  
+                    data-aos-easing="linear" 
+                    data-aos-duration="500">
+                <Typography component="h2" variant="display2" gutterBottom style={{color:'black'}}>
+                    Sign Up To Be A Buddy Today
+                </Typography>
+                    <form>
+                        < TextField
+                            className = ""
+                            placeholder = "First Name" 
+                            name = 'fName' 
+                        />
 
-                    <br/>
+                        <br/>
 
-                    < TextField
-                        className = ""
-                        placeholder = "Last Name" 
-                        style = {{marginTop:'15px'}}
-                        name = 'lName' 
-                    />
+                        < TextField
+                            className = ""
+                            placeholder = "Last Name" 
+                            style = {{marginTop:'15px'}}
+                            name = 'lName' 
+                        />
 
-                    <br/>
+                        <br/>
 
-                    < TextField
-                        className = ""
-                        type = 'email'
-                        placeholder = "Email Address" 
-                        style = {{marginTop:'15px'}} 
-                        name = 'eAddress' 
-                    />
+                        < TextField
+                            className = ""
+                            type = 'email'
+                            placeholder = "Email Address" 
+                            style = {{marginTop:'15px'}} 
+                            name = 'eAddress' 
+                        />
 
-                    <br/>
+                        <br/>
 
-                    < TextField
-                        className = ""
-                        placeholder = "Password" 
-                        style = {{marginTop:'15px'}}
-                        name = 'password'
-                    />
+                        < TextField
+                            className = ""
+                            placeholder = "Password" 
+                            style = {{marginTop:'15px'}}
+                            name = 'password'
+                        />
 
-                    <br/>
+                        <br/>
 
-                    < TextField
-                        className = ""
-                        placeholder = "Confirm Password" 
-                        style = {{marginTop:'15px'}} 
-                    />
+                        < TextField
+                            className = ""
+                            placeholder = "Confirm Password" 
+                            style = {{marginTop:'15px'}} 
+                        />
 
-                    <br/>
+                        <br/>
 
-                    <Button
-                        type = 'submit'
-                        variant = "outlined"
-                        style = {{marginTop:'15px'}}
-                    >
-                        Sign Me Up!
-                    </Button>
+                        <Button
+                            type = 'submit'
+                            variant = "outlined"
+                            style = {{marginTop:'15px'}}
+                        >
+                            Sign Me Up!
+                        </Button>
 
-                </form>
+                    </form>
 
-                <Link to = '/'>
-                     <Button
-                        type = 'submit'
-                        variant = "outlined"
-                        style = {{marginTop:'15px'}}
-                    >
-                        Sign In Instead
-                    </Button>
-                </Link> 
+                    <Link to = '/'>
+                        <Button
+                            type = 'submit'
+                            variant = "outlined"
+                            style = {{marginTop:'15px'}}
+                        >
+                            Sign In Instead
+                        </Button>
+                    </Link> 
+                </div>
             </div>
-        </div>
-    );
+        );
+    }
 }
 
 export default SignUp;
