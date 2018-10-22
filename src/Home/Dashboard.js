ad812238262313af407630111b1f265ff723966a
Andy Wang <andywang219@yahoo.com>
2018-10-22 17:18:52 -0400

    Updated the style of SignIn and SignUp Page

--- src/Home/Dashboard.js
+++ src/Home/Dashboard.js
@@ -9,22 +9,9 @@ class Dashboard extends Component {
         super(props);
         this.state = {
             loggedIn:'true', // This is no longer necessary
-            name: '',
-            uid: '',
         }
         this.logout = this.logout.bind(this);
         this.checkIfUser = this.checkIfUser.bind(this);
-        this.getUserName = this.getUserName.bind(this);
-        this.fireBaseListener = null;
-    }
-
-    componentDidMount() {
-        this.getUserName();
-    }
-    
-    componentWillUnmount(){
-        this.fireBaseListener && this.fireBaseListener();
-        this.getUserName = undefined;
     }
 
     logout(e){
@@ -42,34 +29,12 @@ class Dashboard extends Component {
         })
     }
 
-    // getUserName() {
-    //     console.log(firebase.auth().currentUser);
-    //     // firebase.database(`users/${firebase.auth().currentUser.uid}`).once('value')
-    //     // .then((snap)=>{
-    //     //     if (snap.val()) {
-    //     //         this.setState({
-    //     //             name: snap.val().name
-    //     //         })
-    //     //         console.log(snap.val().name);
-    //     //     }
-    //     // });
-    // }
-
-    getUserName(){
-        axios.post('http://localhost:3001/api/getUsername').then(response => {
-            this.setState({
-                name: this.name
-            })
-        })
-    }
-
   render() {
     return (
       <div >
         {this.checkIfUser()} {/* Check if user is logged in. If not redirect to login page */}
         <h1>Hello there</h1>
         <Button onClick={this.logout}>Logout</Button>
-        <h1>hello {this.state.name} </h1>
       </div>
     );
   }
