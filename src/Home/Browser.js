2effc814736695fcd9050763b99dd7720eb83fa2
MelvinTham <melvintham1997@gmail.com>
2018-10-24 15:23:55 -0400

    added select subject, and added a hard coded subjects that displays in the form of a card. We'll eventually get the api working and replace it

--- src/Home/Browser.js
+++ src/Home/Browser.js
@@ -3,14 +3,20 @@ import Button from '@material-ui/core/Button';
 import Menu from '@material-ui/core/Menu';
 import MenuItem from '@material-ui/core/MenuItem';
 import Typography from '@material-ui/core/Typography';
+import GridList from '@material-ui/core/GridList';
 import TextField from '@material-ui/core/TextField';
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
 import './Dashboard.css';
+import './Browser.css';
 
 class Browser extends Component {
     constructor(props) {
         super(props);
         this.state = {
-          school:''
+          school:'',
+          subject:'',
+          class:'',
         }
         this.handleChange = this.handleChange.bind(this);
     }
@@ -56,17 +62,69 @@ class Browser extends Component {
             },
           ];
 
+          const subject = [
+            {
+                value: '',
+                label: '',
+            },
+            {
+              value: 'Art',
+              label: 'Art',
+            },
+            {
+              value: 'Bio',
+              label: 'Bio',
+            },
+            {
+              value: 'Csc',
+              label: 'Csc',
+            },
+            {
+              value: 'Eco',
+              label: 'Eco',
+            },
+          ];
+          const classes = [
+            
+            {
+              value: 'Csc 103',
+              label: 'Csc 103',
+            },
+            {
+              value: 'Csc 104',
+              label: 'Csc 104',
+            },
+            {
+              value: 'Csc 211',
+              label: 'Csc 211',
+            },
+            {
+              value: 'Csc 212',
+              label: 'Csc 212',
+            },
+            {
+                value: 'Csc 220',
+                label: 'Csc 220',
+              },
+            
+            
+          ];
         const { anchorEl } = this.state;
-    
+
+        console.log(this.state.school);
+        console.log(this.state.subject);
         return (
             <div className="browserTitle"> 
                 <div className = "flexRow">
                     <h1
-                      style = {{color: "black", marginTop: "100px",fontSize:'60px',height:'20vh'}}>Select a Class
+                      style = {{color: "black", marginTop: "100px",fontSize:'60px',height:'15vh'}}
+                      data-aos="fade-down"
+                      data-aos-easing="linear" 
+                      data-aos-duration="400">Select a Class
                     </h1>
                 </div>
 
-                <div style = {{height:'80vh'}}>
+                <div  style = {{height:'65vh'}}>
                 <TextField
                     select
                     label="Select"
@@ -75,44 +133,69 @@ class Browser extends Component {
                     helperText="Please select your school"
                     margin="normal"
                     variant="outlined"
-        >
-          {schools.map(option => (
-            <MenuItem key={option.value} value={option.value}>
-              {option.label}
-            </MenuItem>
-          ))}
-        </TextField>
+                    data-aos="fade-down"
+                    data-aos-easing="linear" 
+                    data-aos-duration="400">
 
-
-
-                {/* <TextField
-                    aria-owns={anchorEl ? 'simple-menu' : null}
-                    aria-haspopup="true"
-                    onClick={this.handleClick}
-                    variant="outlined"
-                >
-                    Select School
+                    {schools.map(option => (
+                    <MenuItem key={option.value} value={option.value}>
+                        {option.label}
+                    </MenuItem>
+                ))}
                 </TextField>
-                <Menu
-                    id="simple-menu"
-                    anchorEl={anchorEl}
-                    open={Boolean(anchorEl)}
-                    onClose={this.handleClose}
+
+                    <br/>
+                 {
+                    this.state.school !== ''?
+                    <TextField
+                    select
+                    label="Select"
+                    value={this.state.subject}
+                    onChange={this.handleChange('subject')}
+                    helperText="Please select your Subject"
+                    margin="normal"
+                    variant="outlined"
+                    data-aos="fade-left" 
+                    data-aos-easing="linear" 
+                    data-aos-duration="400">
+
+                {subject.map(op=> tion (
+                    <MenuItem key={option.value} value={option.value}>
+                        {option.label}
+                    </MenuItem>
+                ))}
+                </TextField>: null
+
+                 }
+                 
+                 <br/>
+
+                 {
+                    this.state.subject == 'Csc'?
+                    <div className="flexRow" data-aos="fade-down" data-aos-easing="linear" data-aos-duration="500">
+                       <GridList  cols={3} padding={150} >
+
+                {classes.map(option => (
+            
+                    <Card key={option.value} value={option.value} className ="flexRow" style={{width:'250px',height:'250px',margin:'10px 10px'}}>
+                        <CardContent >
+                            <Typography variant ="headline">
+                            {option.label}
+                            </Typography >
+                        </CardContent>
+                    </Card>
+                   
                     
-                >
-                    <MenuItem onClick={this.handleClose}>School Name</MenuItem>
-                    <MenuItem onClick={this.handleClose}>School Name</MenuItem>
-                    <MenuItem onClick={this.handleClose}>School Name</MenuItem>
-                </Menu>
-                <br/>
-                    <form class="form-inline">
-                    <input class="form-control mr-sm-1" type="search" placeholder="Search" aria-label="Search"/>
-                    <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search </button>
-                    </form>  */}
+                ))}
+                 </GridList>
+                </div> : null
+
+                 }
+
+                
                 </div>
               
-                
-        
+            
             </div>
         )
     }
