f4e7e1c96d223b71daa955855eefbf0be607fb23
Sunny Mei <meisunny1992@hotmail.com>
2018-10-30 16:20:51 -0400

    newly updated and refactored the addCourses function and get courses.

--- server/controller/userData.ctrl.js
+++ server/controller/userData.ctrl.js
@@ -39,35 +39,46 @@ module.exports = {
         //user is for uid
         var user = firebase.auth().currentUser;
         //ref is the path for uid
-        var ref = firebase.database().ref("users/" + user.uid);
-        //updatedCourse holds "" for now
-        var updatedCourse = "";
-        //perform this function once
+        var ref = firebase.database().ref("users/" + user.uid + "/courseList");
+
+        function constructjson(jsonKey, jsonValue){
+            var jsonObj = {};
+            jsonObj[jsonKey] = jsonValue;
+            return jsonObj;
+        }
         ref.once("value", function (snapshot) {
-            //test holds the current snapshot
-          var test = snapshot.val();
-          //how snapshot looks like
-          console.log(snapshot.val());
-          //accessing snapshot's child by using . operator
-          console.log(test.courseList);
-          //if courseList is empty
-          if (test.courseList === "") {
-              //updatedCourse becomes what we get from the front end
-              updatedCourse = course;
-          }
-          //else, there are already courses stored in courselist, which means we should append courses to the current list
-          else {
-              updatedCourse = test.courseList.concat(",", course);
-          }
+            console.log(String(snapshot.numChildren()))
+            let key = String(snapshot.numChildren());
 
-          console.log("stuff input to database inside ref.on " + updatedCourse);
-          ref.update
-              ({
-                  "courseList": updatedCourse
-              });
-
-        });
-
+            ref.update(constructjson(key,course));
+        })
+        //perform this function once
+        //***************************************
+        // ref.once("value", function (snapshot) {
+        //     //test holds the current snapshot
+        //   var test = snapshot.val();
+        //   //how snapshot looks like
+        //   console.log(snapshot.val());
+        //   //accessing snapshot's child by using . operator
+        //   console.log(test.courseList);
+        //   //if courseList is empty
+        //   if (test.courseList === "") {
+        //       //updatedCourse becomes what we get from the front end
+        //       updatedCourse = course;
+        //   }
+        //   //else, there are already courses stored in courselist, which means we should append courses to the current list
+        //   else {
+        //       updatedCourse = test.courseList.concat(",", course);
+        //   }
+        //
+        //   console.log("stuff input to database inside ref.on " + updatedCourse);
+        //   ref.update
+        //       ({
+        //           "courseList": updatedCourse
+        //       });
+        //
+        // });
+        //********************************************
         //var previousList = ref.
         res.status(201).json("course added");
     },
@@ -76,11 +87,11 @@ module.exports = {
         //var user holds information on the current user
         var user = firebase.auth().currentUser;
         //we want to get data in the table users based on the uid
-        var ref = firebase.database().ref("users/" + user.uid);
+        var ref = firebase.database().ref("users/" + user.uid + "/courseList");
 
         ref.once("value", function(snapshot) {
-        console.log({courseList : snapshot.val()});
-        res.status(201).json({courseList : snapshot.val().courseList})
+        console.log(snapshot.val());
+        res.status(201).json(snapshot.val());
         }, function (errorObject) {
         console.log("The read failed: " + errorObject.code);
         });
