fa978cec8f1f1a6b424fa5d502ba03b5e72a64d5
Matthew Jacome <matthew.10.318@gmail.com>
2018-11-14 15:40:37 -0500

    Reverting changes

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -42,12 +42,19 @@ class Home extends Component {
 
   login(e){
     e.preventDefault();
-
-    firebase.auth().signInWithEmailAndPassword(this.state.email, this.state.password)
-    .catch(function(err) {
-      alert(err)
+    var info = { // JSON object to pass to the api call
+      email: this.state.email,
+      password: this.state.password
+    };
+    axios.post('https://triple-bonito-221722.appspot.com/api/logIn', info) // URL of api call and object being passed to it
+    .then(response => {
+      // This simply creates an alert saying successfully logged in and the user ID.
+      // Should route to different page such as homepage
+      this.props.history.push('/dashboard');
+    })
+    .catch(error => {
+      alert(error.response.data.message); // alert to display error
     });
-    this.props.history.push('/dashboard');
   }
 
   render() {
