05c5a6870abcf7c873435d9a2c4e359a16bef65a
Matthew Jacome <matthew.10.318@gmail.com>
2018-11-24 13:57:34 -0500

    Changed function names

--- server/routes/QuestionAnswer.js
+++ server/routes/QuestionAnswer.js
@@ -2,26 +2,26 @@ const QuestionAnswerController = require('./../controller/QuestionAnswer.ctrl')
 
 module.exports = (router) => {
     router
-    .route('/McreateQuestion') // function url used by frontend
-    .post(QuestionAnswerController.McreateQuestion) // function type [GET PUSH DELETE] and reference
+    .route('/createQuestion') // function url used by frontend
+    .post(QuestionAnswerController.createQuestion) // function type [GET PUSH DELETE] and reference
 
     router
     .route('/deleteQuestion') // function url used by frontend
     .post(QuestionAnswerController.deleteQuestion)
 
     router
-    .route('/MgetQuestions') // function url used by frontend
-    .post(QuestionAnswerController.MgetQuestions)
+    .route('/getQuestions') // function url used by frontend
+    .post(QuestionAnswerController.getQuestions)
 
     router
-    .route('/MgetSingleQuestion') // function url used by frontend
-    .post(QuestionAnswerController.MgetSingleQuestion)
+    .route('/getSingleQuestion') // function url used by frontend
+    .post(QuestionAnswerController.getSingleQuestion)
 
     router
-    .route('/MsubmitAnswer') // function url used by frontend
-    .post(QuestionAnswerController.MsubmitAnswer)
+    .route('/submitAnswer') // function url used by frontend
+    .post(QuestionAnswerController.submitAnswer)
 
     router
-    .route('/MgetReplies') // function url used by frontend
-    .post(QuestionAnswerController.MgetReplies)
+    .route('/getReplies') // function url used by frontend
+    .post(QuestionAnswerController.getReplies)
 }
