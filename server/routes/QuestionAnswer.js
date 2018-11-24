fbf176b638d6aa966f02818b8bd238c15bf8d564
Matthew Jacome <matthew.10.318@gmail.com>
2018-11-23 22:07:27 -0500

    Deleted unused functions

--- server/routes/QuestionAnswer.js
+++ server/routes/QuestionAnswer.js
@@ -2,20 +2,12 @@ const QuestionAnswerController = require('./../controller/QuestionAnswer.ctrl')
 
 module.exports = (router) => {
     router
-    .route('/createQuestion') // function url used by frontend
-    .post(QuestionAnswerController.createQuestion) // function type [GET PUSH DELETE] and reference
-
-    router
     .route('/McreateQuestion') // function url used by frontend
     .post(QuestionAnswerController.McreateQuestion) // function type [GET PUSH DELETE] and reference
 
     router
     .route('/deleteQuestion') // function url used by frontend
     .post(QuestionAnswerController.deleteQuestion)
-
-    router
-    .route('/getQuestions') // function url used by frontend
-    .post(QuestionAnswerController.getQuestions)
 
     router
     .route('/MgetQuestions') // function url used by frontend
@@ -26,16 +18,8 @@ module.exports = (router) => {
     .post(QuestionAnswerController.MgetSingleQuestion)
 
     router
-    .route('/sumbitAnswer') // function url used by frontend
-    .post(QuestionAnswerController.sumbitAnswer)
-
-    router
     .route('/MsubmitAnswer') // function url used by frontend
     .post(QuestionAnswerController.MsubmitAnswer)
-
-    router
-    .route('/getReplies') // function url used by frontend
-    .post(QuestionAnswerController.getReplies)
 
     router
     .route('/MgetReplies') // function url used by frontend
