177d8434ef131ac3284cabd7a7f5328662564669
Matthew Jacome <matthew.10.318@gmail.com>
2018-11-23 22:05:02 -0500

    Adding backend tests

--- test/test-server.js
+++ test/test-server.js
@@ -39,190 +39,69 @@ describe('getUsername', function() {
          done();
       });
    });
-});
-/*
-describe('getEmail', function() {
-    it('should return an error since no user is logged in', function(done) {
-        chai.request(server)
-        .get('/api/getUseremail')
-        .end(function(err, res){
-            res.should.have.status(400);
-            res.should.be.json;
-            res.body.should.have.property('message');
-            res.body.message.should.be.a('string');
-            console.log(res.body.message);
-            done();
-        });
-    });
-});
 
-describe('getUserCourses', function() {
-    it('should return an error since no user is logged in', function(done) {
-        chai.request(server)
-        .get('/api/getUserCourses')
-        .end(function(err, res){
-            res.should.have.status(400);
-            res.should.be.json;
-            res.body.should.have.property('message');
-            res.body.message.should.be.a('string');
-            console.log(res.body.message);
-            done();
-        });
-    });
-});
+   it('should return array with the list of user courses', function(done) {
+      chai.request(server)
+      .post('/api/getUserCourses')
+      .send({
+         id : "HnXfCHwV7xXZcsIsMgsKgl27CXG2"
+      })
+      .end(function(err, res){
+         res.should.have.status(200);
+         res.should.be.json;
+         res.body.should.have.property('courseList');
+         res.body.courseList.should.be.a('array');
+         console.log(res.body.courseList)
+         done();
+      });
+   });
 
-describe('addCourses', function() {
-    it('should return an error since no user is logged in', function(done) {
-        chai.request(server)
-        .post('/api/addCourses')
-        .send({
-            courseName : "Math"
-        })
-        .end(function(err, res){
-            res.should.have.status(400);
-            res.should.be.json;
-            res.body.should.have.property('message');
-            res.body.message.should.be.a('string');
-            console.log(res.body.message);
-            done();
-        });
-    });
-});
+   it('should add a class to the array and should return a success message', function(done) {
+      chai.request(server)
+      .post('/api/addCourses')
+      .send({
+         id : "HnXfCHwV7xXZcsIsMgsKgl27CXG2",
+         courseName : "math"
+      })
+      .end(function(err, res){
+         res.should.have.status(200);
+         res.should.be.json;
+         res.body.should.have.property('message');
+         res.body.message.should.be.a('string');
+         done();
+      });
+   });
 
-describe('deleteUserCourses', function() {
-    it('should return an error since no user is logged in', function(done) {
-        chai.request(server)
-        .post('/api/deleteUserCourses')
-        .send({
-            courseName : "Math"
-        })
-        .end(function(err, res){
-            res.should.have.status(400);
-            res.should.be.json;
-            res.body.should.have.property('message');
-            res.body.message.should.be.a('string');
-            console.log(res.body.message);
-            done();
-        });
-    });
-});
+   it('should delete a class and should return a success message', function(done) {
+      chai.request(server)
+      .post('/api/deleteUserCourses')
+      .send({
+         id : "HnXfCHwV7xXZcsIsMgsKgl27CXG2",
+         courseName : "math"
+      })
+      .end(function(err, res){
+         res.should.have.status(200);
+         res.should.be.json;
+         res.body.should.have.property('message');
+         res.body.message.should.be.a('string');
+         done();
+      });
+   });
 
-
-describe('getUsername', function() {
-    it('should return the username of user who is logged in', function(done) {
-        chai.request(server)
-        .get('/api/getUsername')
-        .end(function(err, res){
-            res.should.have.status(200);
-            res.should.be.json;
-            res.body.should.have.property('name');
-            res.body.name.should.be.a('string');
-            console.log(res.body.name);
-            done();
-        });
-    });
+   it('should return a list of csc courses', function(done) {
+      chai.request(server)
+      .post('/api/getCourses')
+      .send({
+         schoolName : "CUNY CCNY",
+         subject : "csc"
+      })
+      .end(function(err, res){
+         res.should.have.status(200);
+         res.should.be.json;
+         res.body.should.have.property('courseID');
+         res.body.courseID.should.be.a('array');
+         console.log(res.body.courseID);
+         done();
+      });
+   });
 });
-
-describe('getEmail', function() {
-    it('should return the user email', function(done) {
-        chai.request(server)
-        .get('/api/getUseremail')
-        .end(function(err, res){
-            res.should.have.status(200);
-            res.should.be.json;
-            res.body.should.have.property('email');
-            res.body.email.should.be.a('string');
-            console.log(res.body.email);
-            done();
-        });
-    });
-});
-
-describe('addCourses', function() {
-    it('should return a success message', function(done) {
-        chai.request(server)
-        .post('/api/addCourses')
-        .send({
-            courseName : "Math"
-        })
-        .end(function(err, res){
-            res.should.have.status(200);
-            res.should.be.json;
-            res.body.should.have.property('message');
-            res.body.message.should.be.a('string');
-            console.log(res.body.message);
-            done();
-        });
-    });
-});
-
-describe('getUserCourses', function() {
-    it('should return array containing course Math', function(done) {
-        chai.request(server)
-        .get('/api/getUserCourses')
-        .end(function(err, res){
-            res.should.have.status(200);
-            res.should.be.json;
-            res.body.should.have.property('courseList');
-            res.body.courseList.should.be.a('array');
-            console.log(res.body.courseList);
-            done();
-        });
-    });
-});
-
-describe('deleteUserCourses', function() {
-    it('should return a success message', function(done) {
-        chai.request(server)
-        .post('/api/deleteUserCourses')
-        .send({
-            courseName : "Math"
-        })
-        .end(function(err, res){
-            res.should.have.status(200);
-            res.should.be.json;
-            res.body.should.have.property('message');
-            res.body.message.should.be.a('string');
-            console.log(res.body.message);
-            done();
-        });
-    });
-});
-
-describe('createQuestion', function() {
-    it('Should return a success message.', function(done) {
-        chai.request(server)
-        .post('/api/createQuestion')
-        .send({
-            courseName : "csc101",
-            userQuestion : "How do I print hello world?"
-        })
-        .end(function(err, res){
-            res.should.have.status(200);
-            res.should.be.json;
-            res.body.should.have.property('message');
-            res.body.message.should.be.a('string');
-            console.log(res.body.message);
-            done();
-        });
-    });
-});
-
-describe('getQuestion', function() {
-    it('Should return a json.', function(done) {
-        chai.request(server)
-        .post('/api/getQuestion')
-        .send({
-            courseName : "csc101"
-        })
-        .end(function(err, res){
-            res.should.have.status(200);
-            res.should.be.json;
-            res.body.should.have.property('questions');
-            res.body.questions.should.be.a('array');
-            console.log(res.body.uid);
-            done();
-        });
-    });
-});
-*/
