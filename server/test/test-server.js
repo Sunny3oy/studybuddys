82a74a7037af3fd416b9d0b498b5b26450a26aed
mjacome0 <matthew.10.318@gmail.com>
2018-11-29 23:06:33 -0500

    Adding some functionality for encryption. Also moved the mocha tests folder

--- server/test/test-server.js
+++ server/test/test-server.js
@@ -1,6 +1,6 @@
 var chai = require('chai');
 var chaiHttp = require('chai-http');
-var server = require('../server/app');
+var server = require('../app');
 var should = chai.should();
 var firebase = require('firebase');
 var assert = require('assert');
@@ -8,50 +8,50 @@ var assert = require('assert');
 chai.use(chaiHttp);
 
 describe('getUsername', function() {
-   it('should return username Matthew', function(done) {
+   it('should return username Test', function(done) {
       chai.request(server)
       .post('/api/getUsername')
       .send({
-         id : "HnXfCHwV7xXZcsIsMgsKgl27CXG2"
+         id : "yopRBY8GtqNRCTbQnvS3bQCMD413"
       })
       .end(function(err, res){
          res.should.have.status(200);
          res.should.be.json;
          res.body.should.have.property('name');
          res.body.name.should.be.a('string');
-         assert(res.body.name, "Matthew");
+         assert(res.body.name, "Test");
          done();
       });
    });
 
-   it('should return email matthew@email.com', function(done) {
+   it('should return email test@email.com', function(done) {
       chai.request(server)
       .post('/api/getUseremail')
       .send({
-         id : "HnXfCHwV7xXZcsIsMgsKgl27CXG2"
+         id : "yopRBY8GtqNRCTbQnvS3bQCMD413"
       })
       .end(function(err, res){
          res.should.have.status(200);
          res.should.be.json;
          res.body.should.have.property('email');
          res.body.email.should.be.a('string');
-         assert(res.body.email, "matthew@email.com");
+         assert(res.body.email, "test@email.com");
          done();
       });
    });
 
-   it('should return array with the list of user courses', function(done) {
+   it('should return an array with the list of user courses', function(done) {
       chai.request(server)
       .post('/api/getUserCourses')
       .send({
-         id : "HnXfCHwV7xXZcsIsMgsKgl27CXG2"
+         id : "yopRBY8GtqNRCTbQnvS3bQCMD413"
       })
       .end(function(err, res){
          res.should.have.status(200);
          res.should.be.json;
          res.body.should.have.property('courseList');
          res.body.courseList.should.be.a('array');
-         console.log(res.body.courseList)
+         console.log(res.body.courseList);
          done();
       });
    });
@@ -60,7 +60,7 @@ describe('getUsername', function() {
       chai.request(server)
       .post('/api/addCourses')
       .send({
-         id : "HnXfCHwV7xXZcsIsMgsKgl27CXG2",
+         id : "yopRBY8GtqNRCTbQnvS3bQCMD413",
          courseName : "math"
       })
       .end(function(err, res){
@@ -70,42 +70,57 @@ describe('getUsername', function() {
          res.body.message.should.be.a('string');
          done();
       });
-   });
+  });
 
-   it('should delete a class and should return a success message', function(done) {
+  it('should delete a class and should return a success message', function(done) {
+     chai.request(server)
+     .post('/api/deleteUserCourses')
+     .send({
+        id : "yopRBY8GtqNRCTbQnvS3bQCMD413",
+        courseName : "math"
+     })
+     .end(function(err, res){
+        res.should.have.status(200);
+        res.should.be.json;
+        res.body.should.have.property('message');
+        res.body.message.should.be.a('string');
+        done();
+     });
+  });
+
+   it('should return a list of subjects available at CCNY', function(done) {
       chai.request(server)
-      .post('/api/deleteUserCourses')
+      .post('/api/getSubjects')
       .send({
-         id : "HnXfCHwV7xXZcsIsMgsKgl27CXG2",
-         courseName : "math"
+         collegeName : "CTY01"
       })
       .end(function(err, res){
          res.should.have.status(200);
          res.should.be.json;
-         res.body.should.have.property('message');
-         res.body.message.should.be.a('string');
+         res.body.should.have.property('subjects');
+         res.body.subjects.should.be.a('array');
          done();
-      });
+     });
    });
 
-   it('should return a list of csc courses', function(done) {
+   it('should return a list of sections available at CCNY for subject Computer Science', function(done) {
       chai.request(server)
-      .post('/api/getCourses')
+      .post('/api/getSections')
       .send({
-         schoolName : "CUNY CCNY",
-         subject : "csc"
+         collegeName : "CTY01",
+         subjectName : "Computer Science"
       })
       .end(function(err, res){
          res.should.have.status(200);
          res.should.be.json;
-         res.body.should.have.property('courseID');
-         res.body.courseID.should.be.a('array');
-         console.log(res.body.courseID);
+         res.body.should.have.property('sections');
+         res.body.sections.should.be.a('array');
          done();
-      });
+     });
    });
 });
 
+/*
 describe('getUsersByCourseTaken', function() {
     it('Should return a json.', function(done) {
         chai.request(server)
@@ -123,3 +138,4 @@ describe('getUsersByCourseTaken', function() {
         });
     });
 });
+*/
